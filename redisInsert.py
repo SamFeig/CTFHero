746b2ab4beaf10d7a5a34f49919d9ac4a3993c60
Mark Wilmes <mark.wilmes@colorado.edu>
2019-02-23 20:19:41 -0700

    fix redis insert stuff

--- redisInsert.py
+++ redisInsert.py
@@ -3,7 +3,11 @@ import os
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 print("Connected to redis db")
-challenges = [x[0] for x in os.walk('./CTF-challenges')]
-for chal in challenges:
-    print("Inserting {} into database".format(chal))
-    r.set(chal[2:],os.getcwd() + '/CTF-challenges' + chal[1:])
+ctfs = next(os.walk('./CTF-challenges'))[1]
+print(ctfs)
+for ctf in ctfs:
+    challenges = next(os.walk('./CTF-challenges/' + ctf))[1]
+    print(challenges)
+    for chal in challenges:
+        print("Inserting {} into database".format(os.getcwd() + '/CTF-challenges/' + ctf + "/" + chal))
+        r.set(ctf + chal,os.getcwd() + '/CTF-challenges/' + ctf + "/" + chal)
